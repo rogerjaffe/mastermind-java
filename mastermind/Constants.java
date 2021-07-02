c769aeab540d1f71ef3357898a062abd01e0f5ed
Roger Jaffe <rogerjaffe@gmail.com>
2021-07-02 10:44:43 -0700

    Fix %d formatting

--- mastermind/Constants.java
+++ mastermind/Constants.java
@@ -24,7 +24,7 @@ public class Constants
     public static final String GET_SECRET_CODE_PROMPT = "Enter the new secret code using digits 0-9";
     public static final String GET_PLAYER_NAMES = "Enter the player names separated by a comma: ";
     public static final String ENTER_SECRET = "Player %s: What is your secret number? ";
-    public static final String ENTER_GUESS = "Player %s: Enter a %d-digit guess: ";
+    public static final String ENTER_GUESS = "Player %s: Enter a %s-digit guess: ";
     public static final String SOLVED = "Congratulations %s! You solved the puzzle in %d guesses.";
     public static final String CURRENT_SCORE = "Current score: Player 1: %d | Player 2: %d";
     public static final String GET_NAME_ERROR = "Please enter two names";
